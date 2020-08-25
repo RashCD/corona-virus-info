f681d2d117fea40843fd2fcc78d154fc1fad9ecb
CD <mrrash91@gmail.com>
2020-08-26 00:47:43 +0800

    finish working on country view

--- pages/[country].tsx
+++ pages/[country].tsx
@@ -1,11 +1,25 @@
-import { Button, Flex, Icon, Input, InputGroup, InputRightElement, Stack } from '@chakra-ui/core'
-import { subDays } from 'date-fns'
+import {
+  Button,
+  Divider,
+  Flex,
+  Icon,
+  Input,
+  InputGroup,
+  InputRightElement,
+  Stack,
+  Text,
+} from '@chakra-ui/core'
+import styled from '@emotion/styled'
+import CountryPopulation from 'country-json/src/country-by-population.json'
 import { GetStaticPaths, GetStaticProps } from 'next'
 import Head from 'next/head'
 import { useRouter } from 'next/router'
 import { ChangeEvent, FormEvent, MouseEvent, useState } from 'react'
+import { WorldMap } from 'react-svg-worldmap'
 import Card from '../components/Card'
 import api from '../utils/api'
+import { formatDate } from '../utils/date'
+import { formatNumber } from '../utils/number'
 
 interface IListOfCountries {
   Country: string
@@ -13,25 +27,71 @@ interface IListOfCountries {
   ISO2: string
 }
 
-interface ICountryData {
+interface ICovidStat {
+  NewConfirmed: number
+  TotalConfirmed: number
+  NewDeaths: number
+  TotalDeaths: number
+  NewRecovered: number
+  TotalRecovered: number
+}
+
+interface ICountriesStat extends ICovidStat {
   Country: string
   CountryCode: string
-  Province: string
-  City: string
-  CityCode: string
-  Lat: string
-  Lon: string
-  Confirmed: number
-  Deaths: number
-  Recovered: number
-  Active: number
+  Slug: string
   Date: string
 }
 
-const CountryInfo = (props: { data: ICountryData[]; country: string }): JSX.Element => {
-  const { Active, Confirmed, Deaths, Recovered }: ICountryData = props.data[0]
+interface ISummaryData {
+  Global: ICovidStat
+  Countries: ICountriesStat[]
+}
+
+type CountryPopType = {
+  country: string
+  population: number | null
+}
+
+const FlexContainer = styled(Flex)`
+  div > svg {
+    position: absolute;
+    top: -50px;
+    left: calc(calc(100vw / 2) - calc(500px / 2));
+  }
+`
+
+const CountryInfo = (props: { data: ICountriesStat[]; country: string }): JSX.Element => {
+  const {
+    NewConfirmed,
+    NewDeaths,
+    NewRecovered,
+    TotalConfirmed,
+    TotalDeaths,
+    TotalRecovered,
+    Date: DateLatest,
+  } = props.data[0]
   const router = useRouter()
   const [inputCountry, setInputCountry] = useState(props.country)
+
+  const countryPop = (CountryPopulation as CountryPopType[]).filter((data) =>
+    new RegExp(data.country, 'gi').test(props.data[0].Country)
+  )
+
+  const countryPopFormat = formatNumber(countryPop[0].population ?? 0)
+
+  const MapData = [{ country: props.data[0].CountryCode, value: countryPopFormat }]
+
+  const stylingFunction = () => {
+    return {
+      fill: 'red',
+      fillOpacity: 1,
+      stroke: 'black',
+      strokeWidth: 1,
+      strokeOpacity: 0.2,
+      cursor: 'pointer',
+    }
+  }
 
   const handleClick = (e: MouseEvent<HTMLButtonElement> | FormEvent) => {
     e.preventDefault()
@@ -39,8 +99,26 @@ const CountryInfo = (props: { data: ICountryData[]; country: string }): JSX.Elem
   }
 
   const handleOnChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const sanitizeWord = e.target.value.split(' ').join('-')
+    const smallCap = e.target.value.toLowerCase()
+    const sanitizeWord = smallCap.split(' ').join('-')
     setInputCountry(sanitizeWord)
+  }
+
+  const StackComponent: React.FC = ({ children }): JSX.Element => {
+    return (
+      <Stack
+        isInline
+        spacing={3}
+        w="full"
+        flex={1}
+        py={5}
+        justify="center"
+        align="center"
+        flexWrap="wrap"
+      >
+        {children}
+      </Stack>
+    )
   }
 
   return (
@@ -50,54 +128,77 @@ const CountryInfo = (props: { data: ICountryData[]; country: string }): JSX.Elem
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Flex w="full" h="40vh" bg="blue.300" boxShadow="xl" justify="center" align="flex-end" py={8}>
-        <Stack>
+      <FlexContainer
+        w="full"
+        h="40vh"
+        bg="blue.300"
+        boxShadow="sm"
+        justify="center"
+        align="flex-end"
+        pt={8}
+        mb={5}
+        pos="relative"
+      >
+        {MapData && (
+          <WorldMap
+            valuePrefix="population"
+            size="lg"
+            data={MapData}
+            styleFunction={stylingFunction}
+          />
+        )}
+        <Stack boxShadow="md" pos="absolute" bottom={-48 / 2}>
           <form onSubmit={handleClick}>
-            <InputGroup w="xs">
+            <InputGroup w="xs" size="lg">
               <Input
                 placeholder="Search By Country"
                 value={inputCountry}
                 onChange={handleOnChange}
               />
               <InputRightElement>
-                <Button onClick={handleClick}>
+                <Button onClick={handleClick} size="lg">
                   <Icon name="search-2" />
                 </Button>
               </InputRightElement>
             </InputGroup>
           </form>
         </Stack>
-      </Flex>
-      <Stack
-        isInline
-        spacing={3}
-        w="full"
-        flex={1}
-        py={10}
-        justify="center"
-        align="center"
-        flexWrap="wrap"
-      >
-        <Card title="Active" description={Active} />
-        <Card title="Confirmed" description={Confirmed} />
-        <Card title="Deaths" description={Deaths} />
-        <Card title="Recovered" description={Recovered} />
-      </Stack>
+      </FlexContainer>
+      <StackComponent>
+        <Card title="New Confirmed" description={NewConfirmed} />
+        <Card title="New Deaths" description={NewDeaths} />
+        <Card title="New Recovered" description={NewRecovered} pos="relative">
+          <Text
+            pos="absolute"
+            right={-30}
+            bottom={-30}
+            fontSize="sm"
+            color="gray.600"
+            fontStyle="italic"
+          >
+            * Date as of {formatDate(DateLatest)}
+          </Text>
+        </Card>
+      </StackComponent>
+      <Divider w="80%" display="flex" alignSelf="center" />
+      <StackComponent>
+        <Card title="Total Confirmed" description={TotalConfirmed} />
+        <Card title="Total Deaths" description={TotalDeaths} />
+        <Card title="Total Recovered" description={TotalRecovered} />
+      </StackComponent>
     </Flex>
   )
 }
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
-  const dayBeforeISO = subDays(new Date(), 1).toISOString()
-  const twoDayBeforeISO = subDays(new Date(), 2).toISOString()
-
-  const countryData = await api.get<ICountryData[]>(
-    `/country/${params?.country}?from=${twoDayBeforeISO}&to=${dayBeforeISO}`
+  const summaryData = await api.get<ISummaryData>('/summary')
+  const selectedCountry = summaryData.Countries.filter(
+    (country) => country.Slug === params?.country
   )
 
   return {
     props: {
-      data: countryData,
+      data: selectedCountry,
       country: params?.country,
     },
     revalidate: 1,
