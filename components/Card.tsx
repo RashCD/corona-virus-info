ed5da7b06e47dd69f337fd19e72745d1b8237a8a
CD <mrrash91@gmail.com>
2020-08-26 00:46:41 +0800

    Edit card component to allow number to be formatted

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,20 +1,22 @@
 import { Box, BoxProps, Divider, Text } from '@chakra-ui/core'
+import { formatNumber } from '../utils/number'
 
 interface CardProps extends BoxProps {
   title?: string
   description?: string | number
 }
 
-const Card = ({ title = 'test', description, ...rest }: CardProps): JSX.Element => {
+const Card = ({ title = 'test', description, children, ...rest }: CardProps): JSX.Element => {
   return (
-    <Box p={2} shadow="sm" borderWidth="1px" w="sm" minW={20} minH={20} {...rest}>
+    <Box p={2} shadow="sm" borderWidth="1px" rounded="lg" w="auto" minW="2xs" minH={20} {...rest}>
       <Text textAlign="center" fontSize="md">
         {title}
       </Text>
       <Divider />
       <Text textAlign="center" fontSize="6xl" fontWeight="medium">
-        {description}
+        {typeof description === 'number' ? formatNumber(description) : description}
       </Text>
+      {children}
     </Box>
   )
 }
